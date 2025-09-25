7045440478a7732c1ff8a5313dc44c97c6b63d4c
pauloamr <paulo.emanuel@sempreceub.com>
2025-09-25 10:51:11 -0300

    Update Aula7 25/09 10:51

--- Aulas/Aula7/src/aula7/Principal2.java
+++ Aulas/Aula7/src/aula7/Principal2.java
@@ -2,25 +2,31 @@ package aula7;
 
 import java.util.ArrayList;
 
-public class Principal{
+public class Principal2 {
 	
 	public static void main(String[] args) {
 		
 		Livro l1 = new Livro();
 		l1.setAutor("Stewart");
-		l1.setTitulo("Cálculo 2");
+		l1.setTitulo("Calculo II");
 		l1.setAno(2020);
 		
 		Livro l2 = new Livro();
 		l2.setAutor("Halliday");
-		l2.setTitulo("Física 3");
+		l2.setTitulo("Física III");
 		l2.setAno(2020);
 		
 		ArrayList<Livro> ls = new ArrayList<Livro>();
-		ls.add(l1)
-		ls.add(l2)
+		ls.add(l1);
+		ls.add(l2);
 		
-		Bibilioteca
+		Biblioteca b1 = new Biblioteca(ls);
+		b1.setNome("Vera Lúcia");
+		
+		for (Livro livro : b1.getLivros()) {
+			System.out.println(livro.getTitulo());
+		}
+		
 	}
 
 }
